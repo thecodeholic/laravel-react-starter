cd32eab78107c24f3ef07f19039e0031f44f214d
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-12-06 00:43:36 +0400

    Seed 30 users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(30)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',

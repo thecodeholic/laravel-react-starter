dac0b36e29f333c587552784a21cf8980c4c00f7
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-12-06 19:17:29 +0400

    Sort users by id DESC

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return UserResource::collection(User::query()->latest()->paginate(10));
+        return UserResource::collection(User::query()->orderBy('id', 'desc')->paginate(10));
     }
 
     /**

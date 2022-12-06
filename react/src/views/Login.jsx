2ce20dbd066938f342aa14b919ceddb2535ee10f
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-12-06 19:27:23 +0400

    Show validation errors in login form

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -2,11 +2,13 @@ import {Link} from "react-router-dom";
 import axiosClient from "../axios-client.js";
 import {createRef} from "react";
 import {useStateContext} from "../context/ContextProvider.jsx";
+import { useState } from "react";
 
 export default function Login() {
   const emailRef = createRef()
   const passwordRef = createRef()
-  const {setUser, setToken} = useStateContext()
+  const { setUser, setToken } = useStateContext()
+  const [message, setMessage] = useState(null)
 
   const onSubmit = ev => {
     ev.preventDefault()
@@ -20,6 +22,12 @@ export default function Login() {
         setUser(data.user)
         setToken(data.token);
       })
+      .catch((err) => {
+        const response = err.response;
+        if (response && response.status === 422) {
+          setMessage(response.data.message)
+        }
+      })
   }
 
   return (
@@ -27,6 +35,13 @@ export default function Login() {
       <div className="form">
         <form onSubmit={onSubmit}>
           <h1 className="title">Login into your account</h1>
+
+          {message &&
+            <div className="alert">
+              <p>{message}</p>
+            </div>
+          }
+
           <input ref={emailRef} type="email" placeholder="Email"/>
           <input ref={passwordRef} type="password" placeholder="Password"/>
           <button className="btn btn-block">Login</button>
